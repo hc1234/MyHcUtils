22f9a4393ed92d3f65f8641f18f3ecf1de19a3f2
HuangChao <761752116@qq.com>
2018-04-19 16:50:33 +0800

    第二次更新版本2

--- app/src/main/java/com/hcutils/hcutils/MainActivity.java
+++ app/src/main/java/com/hcutils/hcutils/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i("hc","123");
         return true;
     }
 
