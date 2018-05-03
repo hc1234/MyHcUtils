56b54eca807f92ec0940a698ceb7f8888da14aa4
HuangChao <761752116@qq.com>
2018-05-03 11:09:59 +0800

    5.3号提交

---
+++ app/src/main/java/com/hcutils/hcutils/FragmentDialogActivity.java
@@ -0,0 +1,61 @@
+package com.hcutils.hcutils;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.hcutils.hclibrary.FragmentDialog.HCSetFragmentDialog;
+
+public class FragmentDialogActivity extends AppCompatActivity{
+    TextView showpop;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+        showpop= (TextView) findViewById(R.id.showpop);
+        showpop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+    }
+
+    public void showDialog(){
+        HCSetFragmentDialog hcSetFragmentDialog=HCSetFragmentDialog.Getinstace(getSupportFragmentManager())
+                .setgravity(Gravity.LEFT)
+                .setheight((int)(getScreenHeight(this)))
+                .setwith(300)
+                .setlayout(R.layout.dialog_item)
+                .getview(new HCSetFragmentDialog.Getview() {
+                    @Override
+                    public void getview(View view) {
+
+                    }
+                })
+                .show();
+
+    }
+    /**
+     * 获得屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    public  int getScreenHeight(Context context)
+    {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+}
---
+++ app/src/main/java/com/hcutils/hcutils/UnreadActivty.java
@@ -0,0 +1,83 @@
+package com.hcutils.hcutils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hcutils.hclibrary.views.Unreadview;
+
+public class UnreadActivty extends Activity{
+    Unreadview unreadview;
+    Boolean ishsow=true;
+    Boolean move=false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myunread_layout);
+        unreadview= (Unreadview) findViewById(R.id.unread_view);
+        TextView show= (TextView) findViewById(R.id.show_unreadd);
+        final TextView chege_back= (TextView) findViewById(R.id.chage_back);
+        TextView start_move= (TextView) findViewById(R.id.start_move);
+        EditText edit= (EditText) findViewById(R.id.get_unread);
+        unreadview.getUnreadCallback(new Unreadview.unreadcallbck() {
+            @Override
+            public void unreadmove(TextView v) {
+                unreadview.setUnreadVisibility(View.GONE);
+            }
+        });
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ishsow) {
+                    unreadview.setUnreadVisibility(View.GONE);
+                    ishsow=false;
+                }else {
+                    unreadview.setUnreadVisibility(View.VISIBLE);
+                    ishsow=true;
+                }
+            }
+        });
+        chege_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        start_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!move) {
+                    move=true;
+                    unreadview.setUnreadmove(true);
+                }else{
+                    move=false;
+                    unreadview.setUnreadmove(false);
+                }
+            }
+        });
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                unreadview.setUnreadcount(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+
+            }
+        });
+    }
+}
